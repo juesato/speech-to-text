f76426baa8f77c87fe77bbc6f2e090afe55aa8de
Jonathan Uesato <juesato@mit.edu>
2015-07-17 14:21:04 -0400

    Switched to full-duplex API

--- speech_api.js
+++ speech_api.js
@@ -2,6 +2,8 @@ var http = require("http");
 var request = require("request");
 var fs = require("fs");
 
+var API_KEY = 'AIzaSyDpIPdx2BEmRMkYIF_2PVmnMN6-toj-klA'
+
 function urlEncode(obj) {
   var str = [];
   for(var p in obj)
@@ -11,31 +13,80 @@ function urlEncode(obj) {
   return str.join("&");
 }
 
-var apiUrl = 'https://www.google.com/speech-api/v2/recognize?'
+function genPair() {
+	return parseInt(Math.random() * Math.pow(2, 32)).toString(16);
+}
 
-var params = urlEncode({
+// var apiUrl = 'https://www.google.com/speech-api/v2/recognize?'
+
+// var params = urlEncode({
+// 	'output': 'json',
+// 	'lang': 'en-us',
+// 	'pfilter': 1,
+// 	'maxResults': 3,
+// 	'key': 'AIzaSyDpIPdx2BEmRMkYIF_2PVmnMN6-toj-klA'
+// });
+
+var pair = genPair();
+
+var upstreamUrl = 'https://www.google.com/speech-api/full-duplex/v1/up?';
+var upstreamParams = urlEncode({
 	'output': 'json',
 	'lang': 'en-us',
 	'pfilter': 1,
-	'maxResults': 3,
-	'key': 'AIzaSyDpIPdx2BEmRMkYIF_2PVmnMN6-toj-klA'
+	'key': API_KEY,
+	'client': 'chromium',
+	'pair': pair
 });
-
-var options = {
-	'url': apiUrl + params,
+var upstreamOpts = {
+	'url': upstreamUrl + upstreamParams + '&' + ['continuous', 'interim'].join("&"),
 	'headers': {
 		'content-type': 'audio/x-flac; rate=' + 44100
-	}
-}
+	}	
+};
+
+var downstreamUrl = 'https://www.google.com/speech-api/full-duplex/v1/down';
+var downstreamParams = urlEncode({
+	'pair': pair
+});
+var downstreamOpts = {
+	'url': downstreamUrl + '?' + downstreamParams
+};
+
+// var options = {
+// 	'url': apiUrl + params,
+// 	'headers': {
+// 		'content-type': 'audio/x-flac; rate=' + 44100
+// 	}
+// }
+
 
 var source = fs.createReadStream('test.flac');
 source.on('error', function (error) {console.log("Caught", error);});
 
-
 source.pipe(
-	request.post(options, function(error, res, body) {
+	request.post(upstreamOpts, function(error, res, body) {
+		if (error) {
+			console.log("Error");
+			console.log(error);
+		}
+		console.log("UPSTREAM");
 		console.log(body);
-		console.log("res");
-		console.log(res);
+		// console.log("res");
+		// console.log(res);
 	})
-);
+);	
+
+request.get(downstreamOpts, function(error, res, body) {
+	if (error) {
+		console.log("Error");
+		console.log(error);		
+	}
+
+	console.log("DOWNSTREAM");
+	console.log(body);
+});
+
+console.log("Sent off everything");
+console.log(upstreamOpts);
+console.log(downstreamOpts);
